f30643c0990eefcd104aefc6013dfba7f42b56e7
selmeci <roman.selmeci@infones.com>
2014-05-16 16:50:48 +0200

    Update project version
    Signed-off-by: selmeci <roman.selmeci@infones.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-zeromq-majordomo "0.1.0"
+(defproject clj-zeromq-majordomo "0.1.1"
   :description "Clojure implementation of Majordomo pattern"
   :url "https://github.com/selmeci/clj-zeromq-majordomo"
   :dependencies [[org.clojure/clojure "1.5.1"]
